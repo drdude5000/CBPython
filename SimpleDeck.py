a5a1e32766ea9ba01db2f0b3387469aceb4baa98
drdude5000 <drdude5000@live.com>
2017-02-17 16:41:20 -0500

    Simple Deck

---
+++ SimpleDeck.py
@@ -0,0 +1,43 @@
+# Creation of a simple deck class that will contain the 52 cards for the contract bridge game.
+# Oscar Rodriguez
+from random import shuffle
+class Card:
+    def __init__(self, i, j):
+        # 11=Jack 12=Queen 13=King 14=Ace
+        self.num = i
+        # 0=Club 1=Diamond 2=Heart 3=Spade
+        self.suit = j
+
+class BDeck:
+    def __init__(self):
+        #Contains Cards
+        self.lst = []
+        for i in range(13):
+            for j in range(4):
+                tempcard = Card(i+2, j)
+                self.lst.append(tempcard)
+
+    def shuffle(self):
+        shuffle(self.lst)
+
+    def checktop(self):
+        num = ""
+        suit = ""
+        numlst = ["Two","Three", "Four", "Five", "Six", "Seven", "Eight", "Nine","Ten","Jack","Queen","King","Ace"]
+        suitlst = ["Clubs","Diamonds","Hearts","Spades"]
+
+        for i in range(13):
+            if self.lst[0].num == i + 2:
+                num = numlst[i]
+                break
+        for i in range(4):
+            if self.lst[0].suit == i:
+                suit = suitlst[i]
+                break
+
+        print("Top card is the " + num + " of " + suit + ".")
+
+
+sampledeck = BDeck()
+sampledeck.shuffle()
+sampledeck.checktop()
