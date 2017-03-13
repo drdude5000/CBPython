d1b8a25e6bb07981faa5e000b769a75c3a8cc070
drdude5000 <drdude5000@live.com>
2017-03-13 11:58:02 -0400

    Bidding

--- MainControl.py
+++ MainControl.py
@@ -1,26 +1,48 @@
 from SimpleDeck import *
 from Players import *
 from CardSorter import *
+from TrickSystem import *
+from ScoreSystem import *
 
-#enables prints at certain debug levels 0=off 1=on
-verbose = 0
+# enables prints at certain debug levels 0=off 1=on
+verbose = 1
 
-#The object that will commence the game
+# The object that will commence the game
 class GameStart:
     def __init__(self):
-        #Create the Deck of Cards
+        # Create the Deck of Cards
         self.gamedeck = BDeck()
-        #Shuffles Deck
+        # Shuffles Deck
         self.gamedeck.shuffle()
 
-        #Create Players
+        # Create Players
         self.playerlst = []
         for i in range(4):
             self.playerlst.append(Player())
             self.playerlst[i].designation = i
             #Sorts Hand for the players
-            self.playerlst[i].collectHand(CardSort.cSort(self.gamedeck.giveHand()))
+            self.playerlst[i].collecthand(CardSort.csort(self.gamedeck.givehand()))
 
-#Testing
+        # HANDLE BIDDING HERE
+
+        if verbose:
+            print(self.playerlst[0].checkhand() + "\n")
+
+        # A trick handled here
+        # 1st argument is trump value, 5= NO TRUMP
+        simpletrick = Trick(5, self.playerlst)
+        simpletrick.starttrick()
+
+
+# Testing
+# Assumptions: Human player is always South, human player is never dummy. For now humnan always goes first.
 bgame = GameStart()
-print(bgame.playerlst[0].checkHand())
+
+
+
+# Test Scoring
+if verbose:
+    print("\nIf contract level was 3, contract suit was no trump. and 10 tricks were won:")
+    print(str(Score.scoregame(3,4,10)) + " points")
+
+
